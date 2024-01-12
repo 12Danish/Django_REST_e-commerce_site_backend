58e3a2af9f5df38460825e659da0dc6218e354e2
12Danish <danishabbas2004@gmail.com>
2024-01-12 18:51:23 +0500

    Changed time difference to two months in BuyerListView

--- backend/Buyer/views.py
+++ backend/Buyer/views.py
@@ -4,32 +4,38 @@ from API.mixins import BuyerPermissionMixin
 from API.review_serializers import ReviewSerializer
 from API.models import Product, Review
 from datetime import timedelta
+import logging
 from django.utils import timezone
 
-
-# A simple function to display only the appropriate products within a time field
-def one_month_ago():
-    time = timezone.now() - timedelta(days=30)
-    return time
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 # This view handles sending the data over to the front end to display the lists
+
+
 class BuyerProductListView(generics.ListAPIView):
     serializer_class = ProductListSerializer
-    min_time = one_month_ago()
-
     # This will define the queryset for the display for the buyer
     def get_queryset(self):
         # Getting all the objects from the
-        qs = Product.objects.filter(date_created__gte=self.min_time)
+        qs = Product.objects.filter(date_created__gte=self.one_month_ago())
         # Checking if a category has been defined by the user in the params
         category = self.request.query_params.get('category', None)
+        logger.debug(f'Initial queryset: {qs}')
         # If user has specified the category then getting only the items of that category
         if category:
             qs = qs.filter(category=category)
             return qs
         qs = [product for product in qs if product.popular]
+        logger.debug(f'Final queryset: {qs}')
         return qs
+
+        # A simple function to display only the appropriate products within a time field
+
+    def one_month_ago(self):
+        time = timezone.now() - timedelta(days=60)
+        return time
 
 
 # This view is responsible for retrieving the desired product
@@ -48,4 +54,3 @@ class BuyerPostReviewView(BuyerPermissionMixin, generics.CreateAPIView):
         context = super().get_serializer_context()
         context['product_id'] = self.kwargs.get('pk')
         return context
-
