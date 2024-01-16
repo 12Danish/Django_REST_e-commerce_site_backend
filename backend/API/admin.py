44ca68c980cab10677960ad829750b8130f4b2da
12Danish <danishabbas2004@gmail.com>
2024-01-16 12:45:58 +0500

    Added functionality to view reviews related to product in admin

--- backend/API/admin.py
+++ backend/API/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from .models import Product, Review
 
 
+class ReviewInline(admin.StackedInline):
+    model = Review
+    extra = 0
+
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'title', 'owner', 'category', 'price', 'n_bought', 'discount', 'date_created', 'sale_item', 'sale_price',
         'popular')
+    inlines = [ReviewInline]
 
 
 class ReviewAdmin(admin.ModelAdmin):
